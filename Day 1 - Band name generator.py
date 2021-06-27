6e1a49a6791007ae711747225962c51bae901fe2
FER4S <ferasalatrash4444@gmail.com>
2021-06-27 15:19:44 +0300

    Day 1

---
+++ Day 1 - Band name generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What's name of the city you grew up in?\n")
+
+pet = input("What's your pet's name?\n")
+
+print(f'Your brand name could be {city} {pet}')
